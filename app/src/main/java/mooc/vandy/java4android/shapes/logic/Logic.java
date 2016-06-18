625ab889036fdd0f9d9fbaf97d5927f41d0f7ab1
Eric <ericshen@uw.edu>
2016-06-18 00:31:40 -0700

    made some edits

--- app/src/main/java/mooc/vandy/java4android/shapes/logic/Logic.java
+++ app/src/main/java/mooc/vandy/java4android/shapes/logic/Logic.java
@@ -162,4 +162,65 @@ public class Logic
 
     // TODO -- add your code here
 
+    public static double rectangleArea(double length, double width)
+    {
+        double area;
+        area = length * width;
+        return area;
+    }
+    public static double rectanglePerimeter(double length, double width)
+    {
+        double perimeter;
+        perimeter = 2*length + 2*width;
+        return perimeter;
+    }
+    public static double circleArea(double radius)
+    {
+        double area;
+        area = math.PI * math.pow(radius,2);
+        return area;
+    }
+    public static double circleCircumference(double radius)
+    {
+        double circumfrence;
+        circumfrence = 2*math.PI*radius;
+        return circumfrence;
+    }
+    public static double rightTriangleArea(double base, double height)
+    {
+        double area;
+        area = (base*height)/2;
+        return area;
+    }
+    public static double rightTrianglePerimeter(double base, double height)
+    {
+        double perimeter;
+        perimeter = base + height + (math.sqrt(math.pow(base,2)+math.pow(height,2)));
+        return perimeter;
+    }
+    public static double boxVolume(double length, double width, double depth)
+    {
+        double volume;
+        volume = length*width*depth;
+        return volume;
+    }
+    public static double boxSurfaceArea(double length, double width, double depth)
+    {
+        double surfaceArea;
+        surfaceArea = (2*length*depth)+(2*width*depth);
+        return surfaceArea;
+    }
+    public static double sphereVolume(double radius)
+    {
+        double volume;
+        volume = (4/3)*math.PI*math.pow(radius,2);
+        return volume;
+    }
+    public static double sphereSurfaceArea(double radius)
+    {
+        double surfaceArea;
+        surfaceArea = 4*math.PI*math.pow(radius,2);
+        return surfaceArea;
+    }
+
 }
